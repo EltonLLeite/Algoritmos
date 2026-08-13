a5c7c2b4244c98cb21c92bbe43f5de0fab6166f1
EltonLLeite <elton.leite@ufn.edu.br>
2026-08-13 10:27:34 -0300

    Enhance program with search and sort features

--- codigo/exercicio1.cpp
+++ codigo/exercicio1.cpp
@@ -11,6 +11,9 @@ int main() {
     string nomes[TAMANHO];
     int quantidadeIseridos = 0;
     int opcao;
+    string nome;
+    bool houveTroca = true;
+    string auxiliar;
 
     do
     {
@@ -18,6 +21,8 @@ int main() {
         cout << "1 - Cadastrar nomes" << endl;
         cout << "2 - Listar nomes" << endl;
         cout << "3 - Limpar vetor" << endl;
+        cout << "4 - Pesquisar nome" << endl;
+        cout << "5 - Ordenar nomes" << endl;
         cout << "0 - Encerrar programa" << endl;
         cout << "Opcao: ";
         cin >> opcao;
@@ -33,7 +38,6 @@ int main() {
             }
             else
             {
-                string nome;
                 for (int i = 0; i < TAMANHO; i++)
                 {
                     cout << "Digite o nome: ";
@@ -75,7 +79,47 @@ int main() {
             quantidadeIseridos = 0;
             }
             break;
+        case 4:
+            cout << "Procurar nomes de vetor..." << endl;
+            if (quantidadeIseridos == 0)
+            {
+                cout << "Vetor vazio..." << endl;
+            }
+            else
+            {
+                cout << "Digite o nome para pesquisa: ";
+                getline(cin, nome);
+                int quantidadeLocalizados = 0;
+                for (int i = 0; i < quantidadeIseridos; i++)
+                {
+                    if (nomes[i].find(nome) != std::string::npos)
+                    {
+                        cout << nomes[i] << endl;
+                        quantidadeLocalizados++;
+                    }
+                }
+            }
+            break;
+        case 5:
+            cout << "Ordenando o vetor..." << endl;
+            do
+            {
+                houveTroca = false;
+                for (int i = 0; i < quantidadeIseridos-1; i++)
+                {
+                    if (nomes[i] > nomes[i+1])
+                    {
+                        houveTroca = 1;
+                        auxiliar = nomes[i];
+                        nomes[i] = nomes[i+1];
+                        nomes[i+1] = auxiliar;
+                    }
+                }
+            } while (houveTroca);
+            
+            break;
         }
+        
     } while (opcao != 0);
     
 
