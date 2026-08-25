4dad555178fa01a18fbc281ecdd8d2e258a88568
EltonLLeite <elton.leite@ufn.edu.br>
2026-08-24 21:44:57 -0300

    Add files via upload

--- codigo/exercicio1.cpp
+++ codigo/exercicio1.cpp
@@ -1,127 +1,76 @@
-// Fazer um programa em C ou C++ que preencha um vetor de string com n nomes e exiba-os
-
 #include <iostream>
+#include <vector>
 #include <string>
+#include "structs.h"
 
 using namespace std;
 
-#define TAMANHO 4
-
 int main() {
-    string nomes[TAMANHO];
-    int quantidadeIseridos = 0;
+
     int opcao;
-    string nome;
-    bool houveTroca = true;
-    string auxiliar;
+    vector<Pessoa> pessoas;
+    Pessoa temp;
 
     do
     {
-        cout << "MENU" << endl;
-        cout << "1 - Cadastrar nomes" << endl;
-        cout << "2 - Listar nomes" << endl;
-        cout << "3 - Limpar vetor" << endl;
-        cout << "4 - Pesquisar nome" << endl;
-        cout << "5 - Ordenar nomes" << endl;
-        cout << "0 - Encerrar programa" << endl;
-        cout << "Opcao: ";
+        system("cls");
+        cout << "** MENU **" << endl;
+        cout << "1 - Cadastrar pessoa" << endl;
+        cout << "2 - Listar pessoas" << endl;
+        cout << "0 - Sair" << endl;
         cin >> opcao;
-        fflush(stdin);
 
         switch (opcao)
         {
         case 1:
-            cout << "Cadastrar nomes..." << endl;
-            if (quantidadeIseridos == TAMANHO)
-            {
-                cout << "Vetor lotado...";
-            }
-            else
-            {
-                for (int i = 0; i < TAMANHO; i++)
-                {
-                    cout << "Digite o nome: ";
-                    getline(cin, nome);
-                    fflush(stdin);
-
-                    nomes[quantidadeIseridos] = nome;
-                    quantidadeIseridos++;
-                }
-            }
-            break;
-        case 2:
-            cout << "Listar nomes..." << endl;
-            if (quantidadeIseridos == 0)
-            {
-                cout << "Vetor vazio..." << endl;
-            }
-            else
-            {
-                for (int i = 0; i < quantidadeIseridos; i++)
-                {
-                    cout << nomes[i] << endl;
-                }
-                
-            }
-            break;
-        case 3:
-            cout << "Limpando o vetor..." << endl;
-            if (quantidadeIseridos == 0)
-            {
-                cout << "Vetor vazio..." << endl;
-            }
-            else
-            {
-                for (int i = 0; i < TAMANHO; i++)
-                {
-                    nomes[i] = "";
-                }
-            quantidadeIseridos = 0;
-            }
-            break;
-        case 4:
-            cout << "Procurar nomes de vetor..." << endl;
-            if (quantidadeIseridos == 0)
-            {
-                cout << "Vetor vazio..." << endl;
-            }
-            else
-            {
-                cout << "Digite o nome para pesquisa: ";
-                getline(cin, nome);
-                int quantidadeLocalizados = 0;
-                for (int i = 0; i < quantidadeIseridos; i++)
-                {
-                    if (nomes[i].find(nome) != std::string::npos)
-                    {
-                        cout << nomes[i] << endl;
-                        quantidadeLocalizados++;
-                    }
-                }
-            }
-            break;
-        case 5:
-            cout << "Ordenando o vetor..." << endl;
+            cout << "Cadastrar pessoa..." << endl;
+            cout << "Digite o nome: ";
+            cin.ignore();
+            getline(cin, temp.nome);
             do
             {
-                houveTroca = false;
-                for (int i = 0; i < quantidadeIseridos-1; i++)
+                cout << "Digite sua altura (metros): ";
+                cin >> temp.altura;
+                if (temp.altura < 0.30 || temp.altura > 2.70)
                 {
-                    if (nomes[i] > nomes[i+1])
-                    {
-                        houveTroca = 1;
-                        auxiliar = nomes[i];
-                        nomes[i] = nomes[i+1];
-                        nomes[i+1] = auxiliar;
-                    }
+                    cout << "Altura invalida... Redigite" << endl;
                 }
-            } while (houveTroca);
+            } while (temp.altura < 0.30 || temp.altura > 2.70);
             
+            do
+            {
+                cout << "Digite seu peso (kg): ";
+                cin >> temp.peso;
+                if (temp.peso < 30 || temp.peso > 200)
+                {
+                    cout << "Peso invalido... Redigite" << endl;
+                }
+            } while (temp.peso < 30 || temp.peso > 200);
+
+            pessoas.push_back(temp);
+            break;
+        case 2:
+            cout << "Listar pessoas..." << endl;
+            if (pessoas.size() > 0)
+            {
+                for (int i = 0; i < pessoas.size(); i++)
+                {
+                    cout << "Nome: " << pessoas[i].nome << endl;
+                    cout << "Altura: " << pessoas[i].altura << endl;
+                    cout << "Peso: " << pessoas[i].peso << endl;
+                    cout << "=========================" << endl;
+                }
+            }
+            else
+            {
+                cout << "Nenhuma pessoa registrada..." << endl;
+            }
             break;
         }
-        
+        system("pause");
     } while (opcao != 0);
-    
+       
+    system("cls");
 
-    return 1;
+    return 0;
 }
--- codigo/exercicio2.cpp
+++ codigo/exercicio2.cpp
@@ -1,53 +1,58 @@
-#include <iostream>
+#include <limits>
+#include <iostream> //equivalente ao stdio.h do C
+#include <string>   //equivalente ao string.h do C
 #include <vector>
-#include <string>
+#include "structs.h"
 
 using namespace std;
 
-struct Aluno
-{
-    string nome;
-    int matricula;
-    float nota;
-};
-
-vector<Aluno> alunos(5);
-
-void cadastrarAlunos() {
-    for (int i = 0; i < 5; i++)
-    {
-        cout << "Cadastro do aluno " << i + 1 << endl;
-        cout << "Nome: ";
-        getline(cin, alunos[i].nome);
-
-        cout << "Matricula: ";
-        cin >> alunos[i].matricula;
-
-        cout << "Nota: ";
-        cin >> alunos[i].nota;
-
-        cin.ignore();
-    }
-}
-
-void mostrarAlunos() {
-    for (int i = 0; i < 5; i++)
-    {
-        cout << "Aluno " << i + 1 << endl;
-        cout << "Nome: " << alunos[i].nome << endl;
-        cout << "Matricula: " << alunos[i].matricula << endl;
-        cout << "Nota: " << alunos[i].nota << endl;
-        cout << "======================================" << endl;
-    }    
-}
-
 int main() {
+    Pessoa pessoaTMP;
+    vector<Pessoa> atletas;
+    int quantidadeAtletas;
 
-    cadastrarAlunos();
+    cout << "Quantos atletas quer monitorar o IMC? ";
+    cin >> quantidadeAtletas;
+    
+    // LIMPEZA AQUI: Limpa o '\n' deixado pela leitura da quantidade de atletas
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
 
-    cout << "======================================" << endl;
+    //rotina de persistencia em RAM
+    for (int i = 0; i < quantidadeAtletas; i++ ) {
+        cout << "Nome atleta: ";
+        getline(cin, pessoaTMP.nome);
 
-    mostrarAlunos();
+        do {
+            cout << "Altura (m) : ";
+            cin >> pessoaTMP.altura;
+            if (pessoaTMP.altura < 0.30 || pessoaTMP.altura > 2.70) {
+                cout << "Altura invalida. Redigite.....\n";
+            }
+        } while (pessoaTMP.altura < 0.30 || pessoaTMP.altura > 2.70);
 
-    return 0;
+        do
+        {
+            cout << "Peso (kg):";
+            cin >> pessoaTMP.peso;
+            if (pessoaTMP.peso < 30 || pessoaTMP.peso > 200)
+            {
+                cout << "Peso invalido... Redigite" << endl;
+            }
+        } while (pessoaTMP.peso < 30 || pessoaTMP.peso > 200);
+
+        pessoaTMP.imc = pessoaTMP.peso / (pessoaTMP.altura * pessoaTMP.altura);
+
+        // LIMPEZA AQUI: Limpa o '\n' deixado pela leitura do peso antes do próximo getline
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        atletas.push_back( pessoaTMP );
+    }
+    
+    //rotina de calculo do IMC = peso / (altura × altura)
+    for (int i = 0; i < atletas.size(); i++) {
+        cout << "Nome atleta: " << atletas[i].nome << endl;
+        cout << "IMC: " << atletas[i].imc << endl;
+    }
+
+    return 1;
 }
