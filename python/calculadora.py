fd7f4f0b594e8a584d0347f2bd5e0632670555ce
Elton Leite <elton.leite@ufn.edu.br>
2026-09-24 09:59:29 -0300

    Refactor input handling and operation execution in calculator

--- python/calculadora.py
+++ python/calculadora.py
@@ -2,9 +2,6 @@
 
 import math
 
-a = float(input("Digite o primeiro número: "))
-b = float(input("Digite segundo número: "))
-
 print("\n***MENU DE OPERAÇÕES***")
 print("1 - Adição")
 print("2 - Subtração")
@@ -13,16 +10,38 @@ print("4 - Divisão")
 print("0 - Sair")
 opcao = int(input("Opção: "))
 
-match opcao:
-    case 1:
-        resultado = a + b
-        print(f"{a} + {b} = {resultado}")
-    case 2:
-        resultado = a - b
-        print(f"{a} - {b} = {resultado}")
-    case 3:
-        resultado = a * b
-        print(f"{a} * {b} = {resultado}")
-    case 4:
-        resultado = a / b
-        print(f"{a} / {b} = {resultado}")
+if opcao == 0:
+    print("Encerrando o programa...")
+    exit()
+if opcao > 4:
+    print("Opção invalida...")
+    exit()
+
+a = float(input("Digite o primeiro número: "))
+b = float(input("Digite segundo número: "))
+
+#match opcao:
+#    case 1:
+#        resultado = a + b
+#        print(f"{a} + {b} = {resultado}")
+#    case 2:
+#        resultado = a - b
+#        print(f"{a} - {b} = {resultado}")
+#    case 3:
+#        resultado = a * b
+#        print(f"{a} * {b} = {resultado}")
+#    case 4:
+#        resultado = a / b
+#        print(f"{a} / {b} = {resultado}")
+if opcao == 1:
+    resultado = a + b
+    print(f"{a} + {b} = {resultado}")
+elif opcao == 2:
+    resultado = a - b
+    print(f"{a} - {b} = {resultado}")
+elif opcao == 3:
+    resultado = a * b
+    print(f"{a} * {b} = {resultado}")
+elif opcao == 4:
+    resultado = a / b
+    print(f"{a} / {b} = {resultado}")
